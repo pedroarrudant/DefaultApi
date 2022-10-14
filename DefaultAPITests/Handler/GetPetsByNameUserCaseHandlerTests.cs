bdc08ef2b27a3c28f389c23b0f50106ad7f19b6d
Pedro <pedro.arruda@outlook.com.br>
2022-10-14 13:40:17 -0300

    Adicionando TUs

---
+++ DefaultAPITests/Handler/GetPetsByNameUserCaseHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.GetPetsByName.Models;
+using Application.Features.GetPetsByName.UseCase;
+using Application.Shared.Repositories;
+using DefaultAPITests.Static;
+using Moq;
+
+namespace DefaultAPITests.Handler;
+
+public class GetPetsByNameUseCaseHandlerTests
+{
+    private readonly Mock<IPetsRepository> _repository;
+
+    public GetPetsByNameUseCaseHandlerTests()
+    {
+        _repository = new Mock<IPetsRepository>();
+    }
+
+    [Fact]
+    public async void GetPetsByNameShouldPass()
+    {
+        //Arrange
+        _repository.Setup(m => m.GetPetByNameAsync(It.IsAny<string>(), new CancellationToken())).ReturnsAsync(PetStatic.Create());
+        var input = new GetPetsByNameInput() { Name = "Teste" }; 
+
+        //Act
+        var handler = new GetPetsByNameUseCaseHandler(_repository.Object);
+        var result = await handler.Handle(input, new CancellationToken());
+
+        //Assert
+        Assert.NotNull(result.Pet);
+    }
+}
---
+++ DefaultAPITests/Static/PetStatic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Application.Shared.Models;
+
+namespace DefaultAPITests.Static
+{
+    public static class PetStatic
+    {
+        public static Pet Create() => new Pet() { Age = 10, Name = "Teste" };
+    }
+}
+
